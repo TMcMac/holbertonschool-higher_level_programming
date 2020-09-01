6c746ebfaf752f15d15a81f57f063a3686691059
TMcMac <timmcmackenjr@gmail.com>
2020-09-01 20:54:35 +0000

    Add abs() to digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = number % 10
+digit = abs(number) % 10
 print("Last digit of {} is {} and is".format(number, digit), end=' ')
 if (digit == 0):
     print("0")
