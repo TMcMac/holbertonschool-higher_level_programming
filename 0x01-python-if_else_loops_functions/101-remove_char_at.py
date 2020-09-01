bad6a6bd0c85917769b69a126a53de304000a752
TMcMac <timmcmackenjr@gmail.com>
2020-09-01 19:53:57 +0000

    Adds an else statement

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
-
 def remove_char_at(str, n):
     new_str = ""
     for i in range(0, len(str)):
         if i != n:
             new_str += str[i]
-
-    return(new_str)
+        else:
+            pass
+    return new_str
