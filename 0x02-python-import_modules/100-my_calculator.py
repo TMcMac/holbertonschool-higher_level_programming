7b96eaf25c4d31a1add9be48e4987e32950bad61
TMcMac <timmcmackenjr@gmail.com>
2020-09-03 20:37:50 +0000

    Updates the conditional

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,24 +4,21 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-    ops = ["+", "-", "*", "/"]
-
-    if __name__ == "__main__":
-        if len(argv) is not 4:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
-            exit(1)
-        elif str(argv[2]) not in ops:
+    if len(argv) is not 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        op = str(argv[2])
+        if op is '+':
+            print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
+        elif op is '-':
+            print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
+        elif op is '/':
+            print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
+        elif op is '*':
+            print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
+        else:
             print("Unknown operator. Available operators: +, -, * and / ")
             exit(1)
-        else:
-            a = int(argv[1])
-            b = int(argv[3])
-            op = argv[2]
-            if op is '+':
-                print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
-            elif op is '-':
-                print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
-            elif op is '/':
-                print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
-            else:
-                print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
