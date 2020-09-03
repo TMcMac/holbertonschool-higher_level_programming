bffe13ee9c14b0b4ded5accdf6a3cba5f7e918c5
TMcMac <timmcmackenjr@gmail.com>
2020-09-03 17:51:56 +0000

    Adds import guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-import sys
-i = 0
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
+if __name__ == "__main__":
+    import sys
+    i = 0
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
 
-while i < len(sys.argv):
-    if i > 0:
-        print("{}: {}".format(i, sys.argv[i]))
-    i += 1
+    while i < len(sys.argv):
+        if i > 0:
+            print("{}: {}".format(i, sys.argv[i]))
+        i += 1
