99a958230f942e8407df42f76214931af286302a
TMcMac <timmcmackenjr@gmail.com>
2020-09-03 17:49:58 +0000

    Changes the import to call mods by name

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-import calculator_1 as math
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, math.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, math.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, math.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, math.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
