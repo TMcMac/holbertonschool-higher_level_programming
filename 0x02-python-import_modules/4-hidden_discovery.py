46d3d1c9fed8dbc835f892880ef7ab0f74f5618b
TMcMac <timmcmackenjr@gmail.com>
2020-09-03 17:53:27 +0000

    adds imports guard

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-import hidden_4
+if __name__ == "__main__":
 
-names = dir(hidden_4)
-names.sort()
-for name in names:
-    if '__' not in name:
-        print(name)
+    import hidden_4
+
+    names = dir(hidden_4)
+    names.sort()
+    for name in names:
+        if '__' not in name:
+            print(name)
