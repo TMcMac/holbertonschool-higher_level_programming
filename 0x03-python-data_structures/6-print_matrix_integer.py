7aeba323a509d10ec1dec7432d4e2ebae7b3fb83
Tim <timmcmackenjr@gmail.com>
2020-09-08 10:37:43 -0500

    Fixes print format on 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    count = 1
     for row in matrix:
         for num in row:
             print("{}".format(num), end='')
+            if count < len(row):
+                print(" ", end='')
+            count += 1
         print()
+        count = 1
