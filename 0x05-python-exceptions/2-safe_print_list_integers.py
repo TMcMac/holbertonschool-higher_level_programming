78478d333d6e15c751066ab7af9822505cb84d70
Tim <timmcmackenjr@gmail.com>
2020-09-21 15:42:22 -0500

    Adds project

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    passed = 0
+    while count < x:
+        try:
+            print("{:d}".format(my_list[count]), end="")
+        except (ValueError, TypeError):
+            passed += 1
+            pass
+        except IndexError:
+            return
+        count += 1
+    print()
+    return (count - passed)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    newlist = []
+    while i < list_length:
+        try:
+            newlist.append((my_list_1[i] / my_list_2[i]))
+        except TypeError:
+            print("wrong type")
+            newlist.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            newlist.append(0)
+        except IndexError:
+            print("out of range")
+            newlist.append(0)
+        finally:
+            i += 1
+    return newlist
