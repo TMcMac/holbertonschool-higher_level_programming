0c75f112df26d6daf02f55e21a4c1f61ab2bb97b
TMcMac <timmcmackenjr@gmail.com>
2020-09-25 17:51:04 +0000

    Pep8 fixes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,15 +3,15 @@
 
 
 def add_integer(a, b=98):
-    """ 
+    """
     add_integer - a function to add two numbers
     parameters can only be ints or floats
     arg1: an int or a float
     arg2: an int of a float, default 98
     return: the sum of int(arg1) + int(arg2)
     """
-    if not isinstance(a, int) and  not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
-    elif not isinstance(b, int) and  not isinstance(b, float):
+    elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
     return (int(a) + int(b))
