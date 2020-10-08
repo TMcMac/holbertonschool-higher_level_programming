0d00f1f755e9e9726b4d11be60c6811bfaa92364
Tim <timmcmackenjr@gmail.com>
2020-10-08 12:12:14 -0500

    Adds tri

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def pascal_triangle(n=0):
+    triangle = []
+    if n <= 0:
+        return triangle
+    triangle.append([1])
+    row_count = 1
+    while row_count < n:
+        new_row = []
+        i = 0
+        while i < len(triangle[row_count - 1]):
+            if i == 0:
+                new_row.append(1)
+            else:
+                new_row.append(triangle[row_count - 1][i] + triangle[row_count - 1][i - 1])
+            i += 1
+        new_row.append(1)
+        triangle.append(new_row)
+        row_count += 1
+    return triangle
