90f74da9dd056d14f6ab37da1ecf01067e14deec
TMcMac <timmcmackenjr@gmail.com>
2020-10-08 17:18:28 +0000

    fixes long line

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -18,7 +18,8 @@ def pascal_triangle(n=0):
             if i == 0:
                 new_row.append(1)
             else:
-                new_row.append(triangle[row_count - 1][i] + triangle[row_count - 1][i - 1])
+                new_row.append(triangle[row_count - 1][i] +
+                               triangle[row_count - 1][i - 1])
             i += 1
         new_row.append(1)
         triangle.append(new_row)
