a9e977941de346418b0f4493cedab20b0dc8babd
Tim <timmcmackenjr@gmail.com>
2020-10-13 14:30:26 -0500

    Through 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """A base class"""
-import json
 
 
 class Base():
@@ -20,16 +19,37 @@ class Base():
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Returns the JSON string representation of
         list_dictionaries, a list of dictionaries
         """
+        import json
+
         if list_dictionaries is not None:
             if type(list_dictionaries) is list:
-                if list_dictionaries == "":
+                if list_dictionaries == "" or len(list_dictionaries) == 0:
                     return "[]"
                 else:
                     return json.dumps(list_dictionaries)
         else:
             return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Gets the JSON string rep of an obj
+        and saves it to a file with the name 
+        of the class
+        param1: a list of objects
+        """
+        if list_objs is None:
+            list_objs = []
+        fname = cls.__name__ + ".json"
+        list_dictionaries = []
+        for obj in list_objs:
+            list_dictionaries.append(obj.to_dictionary())
+        
+        with open(fname, 'w') as f:
+            f.write(cls.to_json_string(list_dictionaries))
