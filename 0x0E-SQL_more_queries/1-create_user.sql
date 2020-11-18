c24eb9ba55d5864d6d36574a460b82c064eb9ac0
TMcMac <timmcmackenjr@gmail.com>
2020-11-18 03:52:35 +0000

    Initial commit

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create user user_0d_1 and set password, then grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON * . * TO 'user_0d_01'@'localhost';
