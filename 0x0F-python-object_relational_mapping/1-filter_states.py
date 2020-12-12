a1ec06efe57e606f8cb3e04e418b0d7640dbafb4
TMcMac <timmcmackenjr@gmail.com>
2020-12-12 21:45:53 +0000

    Adds 1 and 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+A simple scriupt to connect to a MySQLdb and pull states whose names
+start with the letter n.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    un = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    my_host = "localhost"
+
+    try:
+        db = MySQLdb.connect(host=my_host, port=3306, user=un, passwd=p, db=d)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY 'id' ASC")
+        states = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [%d]: %s", e.args[0], e.args[1])
+        except IndexError:
+            print("MySQL Error: %s", str(e))
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+A simple script to connect to MySQL and pull all states that match
+user supplied name
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    un = sys.argv[1]
+    pw = sys.argv[2]
+    dbase = sys.argv[3]
+    state = sys.argv[4]
+    lhost = "localhost"
+
+    try:
+        db = MySQLdb.connect(host=lhost, port=3306, user=un, passwd=pw, db=dbase)
+        cur = db.cursor()
+        cur.execute("""SELECT * FROM states WHERE name = '{}' ORDER BY id ASC""".format(state))
+        states = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [%d]: %s", e.args[0], e.args[1])
+        except IndexError:
+            print("MySQL Error: %s", str(e))
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
