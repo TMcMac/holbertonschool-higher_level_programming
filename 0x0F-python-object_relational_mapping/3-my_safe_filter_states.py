623e96a58fde77008f0e40f3482807824025b9b0
Tim <timmcmackenjr@gmail.com>
2020-12-12 16:36:46 -0600

    pep8 clean up

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,31 +5,31 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    ''' Connects to a DB and gets info '''
+	''' Connects to a DB and gets info '''
 
-    my_user = sys.argv[1]
-    my_pass = sys.argv[2]
-    my_db = sys.argv[3]
-    passedname = sys.argv[4]
-    localhost = "localhost"
+	my_user = sys.argv[1]
+	my_pass = sys.argv[2]
+	my_db = sys.argv[3]
+	passedname = sys.argv[4]
+	localhost = "localhost"
 
-    try:
-        db = MySQLdb.connect(host=localhost, port=3306, user=my_user,
-                             passwd=my_pass, db=my_db)
-        cur = db.cursor()
-        cur.execute("""SELECT * FROM states
-                       WHERE name = %s
-                       ORDER BY 'id' ASC""", (passedname,))
-        states = cur.fetchall()
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: {}".format(str(e)))
+	try:
+		db = MySQLdb.connect(host=localhost, port=3306, user=my_user,
+							 passwd=my_pass, db=my_db)
+		cur = db.cursor()
+		cur.execute("""SELECT * FROM states
+					   WHERE name = %s
+					   ORDER BY 'id' ASC""", (passedname,))
+		states = cur.fetchall()
+	except MySQLdb.Error as e:
+		try:
+			print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+		except IndexError:
+			print("MySQL Error: {}".format(str(e)))
 
-    for state in states:
-        print(state)
+	for state in states:
+		print(state)
 
-    cur.close()
-    db.close()
+	cur.close()
+	db.close()
 
