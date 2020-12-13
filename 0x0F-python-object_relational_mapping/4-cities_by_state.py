da8bec0a72f6690fab969ea49d34dce1847a01c9
TMcMac <timmcmackenjr@gmail.com>
2020-12-13 19:17:56 +0000

    Adds 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""
+For this script we will be combining two tables
+to get cities with their states
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    arguments will be username, password
+    database name. Results will be sorted
+    by city id
+    """
+    un = sys.argv[1]
+    pw = sys.argv[2]
+    dbase = sys.argv[3]
+    my_host = "localhost"
+
+    try:
+        db = MySQLdb.connect(host=my_host, port=3306, user=un, passwd=pw, db=dbase)
+        cur = db.cursor()
+        cur.execute("""SELECT cities.id, cities.name, states.name
+                       FROM cities
+                       INNER JOIN states ON cities.state_id = states.id
+                       ORDER BY cities.id ASC""")
+        result = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQLdb Error {}: {}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQLdb Error: {}".format(str(e)))
+
+    for city in result:
+        print(city)
+
+    cur.close()
+    db.close()
