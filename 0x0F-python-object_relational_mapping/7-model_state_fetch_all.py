1e39b36295b0666084556e7c61142154559be590
TMcMac <timmcmackenjr@gmail.com>
2020-12-14 03:17:10 +0000

    Adds 7 and pep8 fixes

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Use SQL alchemy to get all states from the states table"""
+
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+
+def get_states():
+    engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                                      .format(argv[1],
+                                              argv[2],
+                                              argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+if __name__ == '__main__':
+    get_states()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """ defines state with inheritance from base from SQLalchemy """
     __tablename__ = 'states'
