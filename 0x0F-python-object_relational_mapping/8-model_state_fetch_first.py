90c565a9b7feb3bf7e0c8723eecdcdc4b30cc709
TMcMac <timmcmackenjr@gmail.com>
2020-12-14 04:31:51 +0000

    adds 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Gets only the first row in the states table
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+
+
+def get_first_state():
+    """Get the first state in the table"""
+    user_name = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user_name, password, db_name))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).first():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+if __name__ == '__main__':
+    get_first_state()
