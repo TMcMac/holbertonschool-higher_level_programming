8c04be6ec2c0aeb3953d0d2fc61fc706a2047d1e
TMcMac <timmcmackenjr@gmail.com>
2020-12-14 05:00:08 +0000

    Adds 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Get a state by name from the table states"""
+
+
+import sqlalchemy
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def get_a_state():
+    """get a state by name"""
+    user = argv[1]
+    pw = argv[2]
+    db = argv[3]
+    astate = argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(user, pw, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == astate)
+
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not Found")
+
+    session.close()
+
+if __name__ == '__main__':
+    get_a_state()
