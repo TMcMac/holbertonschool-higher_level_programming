74a84658bff513f9649ee890ea13bc2e149f4a1c
TMcMac <timmcmackenjr@gmail.com>
2020-12-14 05:29:13 +0000

    Adds 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+Gets all states from the states table
+"""
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name='Louisiana')
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
+
+    session.close()
+
