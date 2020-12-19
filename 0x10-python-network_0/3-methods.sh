155e2cbe208618970a2b3c9968d1515b4ff20dea
TMcMac <timmcmackenjr@gmail.com>
2020-12-19 03:21:36 +0000

    Cuts allow: off

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS "$1" -i | grep 'Allow:'
+curl -sX OPTIONS "$1" -i | grep 'Allow:' | tail -c +8
