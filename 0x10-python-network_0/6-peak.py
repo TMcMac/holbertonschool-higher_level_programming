3d0ef978c9cb42b0dbe6e97b6c82f3d478ba0051
TMcMac <timmcmackenjr@gmail.com>
2020-12-19 02:52:59 +0000

    The literally simplest solution to 6 but with O(n)

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,24 +11,3 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     else:
         return max(list_of_integers)
-    """
-    if len(list_of_integers) == 2:
-        if list_of_integers[0] > list_of_integers[1]:
-            return list_of_integers[0]
-        else:
-            return list_of_integers[1]
-
-    nums = list_of_integers[:]
-    high = index(max(nums))
-    if high == 0 or high == len(nums) - 1:
-        return nums[high]
-
-    h = 0
-    i = 1
-    j = 2
-    if nums[h] >= nums[i]:
-        peaks.append(nums[h])
-
-    while i < (len(nums) - 2):
-        if nums[h] <= nums[i] >= nums[j]:
-    """
