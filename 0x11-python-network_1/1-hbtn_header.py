a3eda745fc04a5b5d9c1f3203a1e7fd85695c7cd
TMcMac <timmcmackenjr@gmail.com>
2021-01-11 20:13:32 +0000

    Reworks with an include gaurd and check for args

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,11 @@
 '''a Python script that takes in a URL, sends a request
 to the URL and displays the value of the X-Request-Id'''
 
-from sys import argv
-import urllib.request
 
-with urllib.request.urlopen(argv[1]) as response:
-    head = response.getheaders()
-    for item in head:
-        if item[0] == 'X-Request-Id':
-            print(item[1])
+if __name__ == '__main__':
+    from sys import argv
+    import urllib.request
+
+    if argv[1]:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.getheader('X-Request-Id'))
