103984633d36f3fc4a35de357a4ce88a8e657975
TMcMac <timmcmackenjr@gmail.com>
2021-01-11 23:30:32 +0000

    Adds 4 and 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""a Python script that fetches https://intranet.hbtn.io/status"""
+
+if __name__ == '__main__':
+    import requests
+
+    response = requests.get('https://intranet.hbtn.io/status')
+    content = response.text
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(content), content))
