41e13e7701bae90972b265bba7c740d293e83fb6
TMcMac <timmcmackenjr@gmail.com>
2021-01-11 21:31:10 +0000

    pep8 fixes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" 
+"""
 a Python script that takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response
 """
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     from sys import argv
-    from  urllib import request, parse
+    from urllib import request, parse
     value = {'email': argv[2]}
     data1 = parse.urlencode(value)
     data = data1.encode("utf-8")
