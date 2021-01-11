8f73a49b8276ea8bdb9a5ebba96930d3b4bb688b
TMcMac <timmcmackenjr@gmail.com>
2021-01-11 20:03:16 +0000

    Adds 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''a Python script that takes in a URL, sends a request
+to the URL and displays the value of the X-Request-Id'''
+
+from sys import argv
+import urllib.request
+
+with urllib.request.urlopen(argv[1]) as response:
+    head = response.getheaders()
+    for item in head:
+        if item[0] == 'X-Request-Id':
+            print(item[1])
