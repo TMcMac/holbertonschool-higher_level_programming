a8ad4d281632fd7da51e9e7c8fa4d48e1afe065d
TMcMac <timmcmackenjr@gmail.com>
2021-01-11 21:51:46 +0000

    Adds 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response
+"""
+
+if __name__ == '__main__':
+    from liburl import request, error
+    from sys import argv
+
+    try:
+        with request.urlopen(argv[1]) as response:
+            html = response.read()
+            content = html.decode("utf-8")
+            print(content)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
