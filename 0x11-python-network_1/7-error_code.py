e8cab2c0d6045abe17baced5fb4c3d552086e148
TMcMac <timmcmackenjr@gmail.com>
2021-01-12 00:07:30 +0000

    Adds 7 and 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    stat = r.status_code
+    if stat >= 400:
+        print("Error code: {}".format(stat))
+    else:
+        print(r.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {}
+    if argv[2]:
+        data[q] = argv[2]
+    else:
+        data[q] = ""
+    r = requests.post(url, data)
+    try:
+        json_s = r.json()
+        if json_s == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_s['id'], json_s['name']))
+    except Exception as err:
+        print("Not a valid JSON")
