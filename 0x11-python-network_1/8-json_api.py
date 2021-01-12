1cf02fb0fcd98f9baa9d42f951a27f4ff90799da
TMcMac <timmcmackenjr@gmail.com>
2021-01-12 21:39:11 +0000

    Redone

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,21 +5,20 @@ sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import requests
     from sys import argv
-
     data = {}
-    if argv[1]:
+    if len(argv) >= 2:
         data['q'] = argv[1]
     else:
         data['q'] = ""
-    r = requests.post(url, data=data)
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
-        json_s = r.json()
-        if json_s == {}:
+        json_string = response.json()
+        if json_string == {}:
             print("No result")
         else:
-            print("[{}] {}".format(json_s['id'], json_s['name']))
+            print("[{}] {}".format(json_string['id'], json_string['name']))
     except Exception as err:
         print("Not a valid JSON")
